41662cd8fe98b70cc8cd18cd763705aec5c5b993
Yasmi Vizeu <yasminvizeusantana@yahoo.com>
2024-10-16 18:45:10 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-function createNavbar(){
-    
+function createNavebar(){
+    const element = document.getElementById('navbar-local')
+    const navbar = ` 
+    <nav class="nav-top">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="index.html#cards">Galeria</a></li>
+            <li><a href="index.html#agenda">Agenda</a></li>
+            <li><a href="index.html#contato">Contato</a></li>
+            <li><a href="login.html">Entrar</a></li>
+        </ul>
+    </nav>
+    `
+    element.innerHTML = navbar
 }
+
+createNavebar()
